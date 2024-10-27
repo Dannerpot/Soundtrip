61f50c7849fb916f87a73e4f06aa0157687fd611
Dannerpot <119153225+Dannerpot@users.noreply.github.com>
2024-10-27 14:50:00 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,54 +1,18 @@
-import React, { useState } from 'react';
-import SongInputForm from './components/SongInputForm';
-import './App.css'
-const App = () => {
-  const [recommendations, setRecommendations] = useState([]);
-  const [loading, setLoading] = useState(false);
+require('dotenv').config();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-  const getRecommendations = async (songs) => {
-    setLoading(true);
-    try {
-      const response = await fetch('http://localhost:5000/recommend', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ songs }),
-      });
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-      const data = await response.json();
-      setRecommendations(data.recommendations);  // Set recommendations from backend response
-    } catch (error) {
-      console.error("Error fetching recommendations:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  return (
-    <div Class="mainscreen">
-      <div class="subscreen"> 
-      <h1 className='title'>SoundTrip</h1>
-      <p className='header'>Enter up to 3 songs to get music recommendations:</p>
-      <SongInputForm onSubmit={getRecommendations} />
-
-      {loading ? (
-        <p>Loading recommendations...</p>
-      ) : (
-        recommendations.length > 0 && (
-          <div className='results'>
-            <h2>Recommendations:</h2>
-            <ul className='list'>
-              {recommendations.map((song, index) => (
-                <li key={index}>{song}</li>
-              ))}
-            </ul>
-          </div>
-        )
-      )}
-      </div>
-    </div>
-  );
-};
-
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
